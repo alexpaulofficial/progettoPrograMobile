61c8e7391956c5c84d1c5f88c70c781f00fdb2b3
Alessio <github@alexpaulofficial.com>
2021-05-18 11:56:45 +0200

    Sempre il primo upload
    C'è stato un errore in github desktop

---
+++ app/src/main/java/com/example/provamap/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.provamap.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.android.gms.maps.model.LatLng
+
+
+
+
+class HomeViewModel : ViewModel() {
+
+}
